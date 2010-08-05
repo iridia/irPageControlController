c15b34da23e1132fc016580b1cc5b4468b49ffdb
Evadne Wu <ev@monoceroi.com>
2010-08-06 00:40:48 +0800

    Initial commit.  Checks in the scaffold which might work.

---
+++ lib.irPageControlController.js
@@ -0,0 +1,135 @@
+//	iridia.pageControlController.js
+//	Evadne Wu at Iridia, 2010
+
+
+
+
+
+	window.iridia = (window && window.iridia || {});
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	iridia.pageControlController = new JS.Class({
+	
+		extend: {
+		
+			delegateProtocol: new JS.Interface([
+			
+				/* (Boolean) */ "pageControlShouldShowPage",
+				/* (void) */ "pageControlDidShowPage"
+			
+			])
+		
+		},
+	
+		include: JS.Delegatable,
+		
+		"initialize": function (inOptions, inDelegate) {
+		
+			this.options = $.extend(jQuery.kDeepCopyEnabled, {
+			
+				manifestObject: null,
+				manifestObjectChildrenSelectorString: "li",
+				manifestObjectChildrenActiveCSSClass: "active",
+				
+				totalPages: 0,
+				currentPageIndex: 0,
+				hoverredPageIndex: null
+			
+			}, inOptions);
+		
+		},
+		
+		
+		
+		
+		
+		"setCurrentPage": function (inPageIndexToMakeCurrent) {
+		
+			if (typeof inPageIndexToMakeCurrent != "number") return;
+			
+			this.representingObjectForPageIndex(this.options.currentPageIndex).removeClass(
+			
+				this.options.manifestObjectChildrenActiveCSSClass
+			
+			);
+			
+			if (inPageIndexToMakeCurrent > this.options.totalPages)
+			this.setTotalPages(inPageIndexToMakeCurrent + 1);
+			
+			this.options.currentPageIndex = inPageIndexToMakeCurrent;
+			
+			this.representingObjectForPageIndex(this.options.currentPageIndex).addClass(
+			
+				this.options.manifestObjectChildrenActiveCSSClass
+			
+			);
+		
+		},
+		
+		
+		
+		
+		
+		"setTotalPages": function (inTotalPagesCount) {
+		
+			if (typeof inTotalPagesCount != "number") return;
+		
+			if (inTotalPagesCount != this.options.totalPages)
+			this.options.manifestObject.empty()
+		
+			this.options.totalPages = inTotalPageCount;
+			
+			//	Current page out of bounds?
+			
+			if (this.options.currentPageIndex > this.options.totalPages)
+			this.setCurrentPage(null);
+		
+		},
+		
+		
+		
+		
+		
+		//! 
+		//!	DOM Helper
+		
+		representingObjectForPageIndex: function (inIndex) {
+		
+			return this.options.manifestObject.children(
+			
+				this.options.manifestObjectChildrenSelectorString
+			
+			).eq(inIndex);
+		
+		},
+		
+		generateManifestObjectChildrens: function () {		
+		
+			this.options.manifestObject.empty();
+			
+			var i = this.options.totalPages; while(i--) {
+			
+				this.options.manifestObject.prepend(
+				
+					$(["<", this.options.manifestObjectChildrenSelectorString, "/>"].join(""))
+				
+				);
+			
+			}
+		
+		}
+	
+	});
+	
+	
+	
+	
+	
